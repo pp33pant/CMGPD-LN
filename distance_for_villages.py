5d065b3902acb194e1ae563a45b2a069418c46d4
Guanghui <panguanghuipant@gmail.com>
2018-09-26 14:29:17 -0400

    Update distance_for_villages.py

--- distance_for_villages.py
+++ distance_for_villages.py
@@ -1,5 +1,7 @@
 # This document shows how I generate the distance matrix of villages. 
 # the la1 and la2 representatively denote the latitude of village 1 and village 2, while the long1 and long2 representatively denote the longitude. 
+import numpy as np
+import pandas as pd 
 def distance(la1,long1,la2,long2):
     la1,long1,la2,long2=map(np.radians,[la1,long1,la2,long2])
     dlong=long1-long2
