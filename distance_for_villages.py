7f55016b7699a9e3d57e8ced0029cd669c5c74bd
Guanghui <panguanghuipant@gmail.com>
2018-10-10 14:21:55 -0400

    Update distance_for_villages.py

--- distance_for_villages.py
+++ distance_for_villages.py
@@ -11,4 +11,17 @@ def distance(la1,long1,la2,long2):
     r = 6371
     distance = c* r *1000
     return distance 
+
 # I record the latitude and longitude of the villages in this dataset (the restricted dataset).
+#and then we calculate the distance decay: 
+
+def decay(distance):
+    if distance==0:
+        distance=1
+    decay=(1/(distance**2))
+    return decay
+
+# after constructing the distance-decay matrix, we could transfer the distance-decay matrix into the distance weight matrix. 
+
+def weight(decay:pd.Series):
+    
